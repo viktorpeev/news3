642a5dc71ae8067312cb4fa3ff1219d887a85f1e
mefani <tashkovastefani@gmail.com>
2021-10-01 12:50:42 +0300

    1.10

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
-
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercises.component";
+import CreateExercise from "./components/create-exercises.component";
+import CreateUser from "./components/create-user.component";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      Hello world!
+     <Router>
+       <Navbar/>
+       <br/>
+       <Route path="/" exact component={ExercisesList}/>
+       <Route path="/edit/:id" exact component={EditExercise}/>
+       <Route path="/create" exact component={CreateExercise}/>
+       <Route path="/user" exact component={CreateUser}/>
+     </Router>
     </div>
   );
 }
---
+++ src/components/edit-exercises.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditExercises extends Component {
+  render(){
+    return(
+      <div>
+        <p>edit exercises component</p>
+      </div>
+    )
+  }
+}
